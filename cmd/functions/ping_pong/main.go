d9afefda6eb8b5acd922bb89dcb4ec8774e7de01
mrz1836 <mrz1818@gmail.com>
2023-03-02 11:05:26 -0500

    Added second function as proof of concept

---
+++ cmd/functions/ping_pong/main.go
@@ -0,0 +1,47 @@
+// Package main is the entry point for the ping_pong lambda function
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"go-api-gateway/configuration" //nolint: gci // This is a local package
+	"log"
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda" //nolint: gci // This is a false positive
+)
+
+// handler is our lambda handler invoked by the `lambda.Start` function call
+func handler(ctx context.Context, r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	// Sanity check
+	log.Println("Ping Pong!")
+
+	// Example using another package
+	log.Println(configuration.Something())
+
+	// Start a segment for X-Ray
+	// var seg *xray.Segment
+	// ctx, seg = xray.BeginSegment(ctx, "marshall-print")
+
+	// Marshal the request
+	j, err := json.Marshal(r)
+	if err != nil {
+		return events.APIGatewayProxyResponse{Body: "request failed", StatusCode: http.StatusBadRequest}, err
+	}
+
+	// Log the request data
+	log.Println(string(j))
+
+	// Close the segment for X-Ray
+	// seg.Close(nil)
+
+	// Response
+	return events.APIGatewayProxyResponse{Body: "Ping Pong!", StatusCode: http.StatusOK}, nil
+}
+
+// main function to launch the lambda
+func main() {
+	lambda.Start(handler)
+}
